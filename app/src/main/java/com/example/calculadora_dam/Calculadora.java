792a4bad47a843245449a186cde4bd4af16cbab3
laura1513 <laurajorques2003@gmail.com>
2023-10-10 15:49:53 +0200

    el resultado es -1 si la operación está mal formulada

--- app/src/main/java/com/example/calculadora_dam/Calculadora.java
+++ app/src/main/java/com/example/calculadora_dam/Calculadora.java
@@ -6,16 +6,20 @@ public class Calculadora {
         String num1;
         String num2;
         int menos;
-        if (cadena.contains("+")) {
-            mas = cadena.indexOf("+");
-            num1 = cadena.substring(0, (mas));
-            num2 = cadena.substring((mas)+1, cadena.length());
-            cadena = Integer.toString(Integer.parseInt(num1)+Integer.parseInt(num2));
-        } else if (cadena.contains("-")) {
-            menos = cadena.indexOf("-");
-            num1 = cadena.substring(0, (menos));
-            num2 = cadena.substring((menos)+1, cadena.length());
-            cadena = Integer.toString(Integer.parseInt(num1)-Integer.parseInt(num2));
+        try {
+            if (cadena.contains("+")) {
+                mas = cadena.indexOf("+");
+                num1 = cadena.substring(0, (mas));
+                num2 = cadena.substring((mas) + 1, cadena.length());
+                cadena = Integer.toString(Integer.parseInt(num1) + Integer.parseInt(num2));
+            } else if (cadena.contains("-")) {
+                menos = cadena.indexOf("-");
+                num1 = cadena.substring(0, (menos));
+                num2 = cadena.substring((menos) + 1, cadena.length());
+                cadena = Integer.toString(Integer.parseInt(num1) - Integer.parseInt(num2));
+            }
+        } catch (Exception e) {
+            cadena = "-1";
         }
         return cadena;
     }
